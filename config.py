9edf83ecbd0b7c0f2d50b69396635602f708d379
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-07-20 16:37:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 import os
 
-API_ID = os.environ.get("23990433")
-API_HASH = os.environ.get("e6c4b6ee1933711bc4da9d7d17e1eb20")
-BOT_TOKEN = os.environ.get("5811431199:AAGad6YjD5nC9PTgurWIbRM4krUpopevpf8")
+API_ID = os.environ.get("API_ID")
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 if not API_ID or not API_HASH or not BOT_TOKEN:
     raise ValueError("API_ID, API_HASH, and BOT_TOKEN must be set")
 
 print(f"API_ID: {API_ID}, API_HASH: {API_HASH}, BOT_TOKEN: {BOT_TOKEN}")
-Make sure you set the environment varia

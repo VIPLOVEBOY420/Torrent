a7f5db38456ff6fd9fe00529272b07ebed67a76c
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-07-20 16:29:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-# (c) @ask_admin001
-
 from pyrogram import Client
 from config import API_HASH, BOT_TOKEN, API_ID
 
-bot = Client(
-    "Web Scrapping Bot",
-    plugins = dict(root="plugins"),
-    bot_token=BOT_TOKEN,
-    api_id=API_ID,
-    api_hash=API_HASH,
-)        
-        
-print("Bot running...")
-bot.run()
+print(f"API_ID: {API_ID}")
+print(f"API_HASH: {API_HASH}")
+print(f"BOT_TOKEN: {BOT_TOKEN}")
+
+try:
+    bot = Client(
+        "Web Scraping Bot",
+        plugins=dict(root="plugins"),
+        bot_token=BOT_TOKEN,
+        api_id=API_ID,
+        api_hash=API_HASH,
+    )
+    
+    print("Bot running...")
+    bot.run()
+except Exception as e:
+    print(f"An error occurred: {e}")

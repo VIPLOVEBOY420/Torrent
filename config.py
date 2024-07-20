1f18caf9b0d38da6adf0568417641e45534022fb
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-07-20 16:12:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-API_ID = os.environ.get("API_ID")
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+API_ID = os.environ.get("23990433")
+API_HASH = os.environ.get("e6c4b6ee1933711bc4da9d7d17e1eb20")
+BOT_TOKEN = os.environ.get("5811431199:AAGad6YjD5nC9PTgurWIbRM4krUpopevpf8")
 
